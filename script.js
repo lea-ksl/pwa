5fac3f0b2e144fe1f79b01aba8c61ffffc5656b1
Léa KSL <semiramoth.lea@gmail.com>
2020-12-17 15:02:29 +0100

    change icons in manifest

--- script.js
+++ script.js
@@ -36,12 +36,7 @@ function afficher(json){
               <div class="card-content">
                 <div class="media">
                   <div class="media-left">
-                    <figure class="image is-48x48">
-                      <img
-                        src="https://giffiles.alphacoders.com/981/98174.gif"
-                        alt="Placeholder image"
-                      />
-                    </figure>
+                    
                   </div>
                   <div class="media-content">
                     <p class="title is-4">${repo.name}</p>
