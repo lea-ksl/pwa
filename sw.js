2b0e9c914985c4326ebe2b7ea5a8c980d50e77e0
Léa KSL <semiramoth.lea@gmail.com>
2021-01-06 22:02:31 +0100

    change sw code

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+window.addEventListener("beforeinstallprompt", e => { 
+    e.preventDefault() ; 
+    deferredPrompt = e ;
+    btn.addEventListener("click", e =>{ 
+        deferredPrompt.promptprompt() ; 
+    }) ; 
+});
+
 self.addEventListener("fetch", event => 
     { console.log(event.request.url); }
 );
